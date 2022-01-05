c33f23816b117f347ebceccb3726a41605688f2e
lemmoor <49235295+lemmoor@users.noreply.github.com>
2022-01-05 21:20:29 +0100

    tracking neighbours

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -8,7 +8,9 @@ class Line {
 }
 
 function Grid({ gridSize }) {
-    let gridCells = [];
+
+    //TODO: make this whole thing a function and just pass in gridSize, return gridLines
+    let gridLines = [];
     for (let i = 0; i < gridSize * 2 + 1; i++) {
         let row = [];
         for (let j = 0; j < gridSize + 1; j++) {
@@ -21,7 +23,33 @@ function Grid({ gridSize }) {
             }
 
         }
-        gridCells.push(row)
+        gridLines.push(row)
+    }
+
+    //tracking neigbours (squares) for each line
+    for (let i = 0; i < gridLines.length; i++) {
+        for (let j = 0; j < gridLines[i].length; j++) {
+            if (gridLines[i][j].isHorisontal) {
+                //checking for bottom square
+                if (gridLines[i + 1]) {
+                    gridLines[i][j].neighbours.push([gridLines[i + 1][j + 1], gridLines[i + 1][j], gridLines[i + 2][j]])
+                }
+                //checking for top square
+                if (gridLines[i - 1]) {
+                    gridLines[i][j].neighbours.push([gridLines[i - 1][j + 1], gridLines[i - 1][j], gridLines[i - 2][j]])
+                }
+            }
+            else {
+                //left square
+                if (gridLines[i][j - 1]) {
+                    gridLines[i][j].neighbours.push([gridLines[i][j - 1], gridLines[i - 1][j - 1], gridLines[i + 1][j - 1]])
+                }
+                //right square
+                if (gridLines[i][j + 1]) {
+                    gridLines[i][j].neighbours.push([gridLines[i][j + 1], gridLines[i - 1][j], gridLines[i + 1][j]])
+                }
+            }
+        }
     }
 
     function drawDots() {
@@ -37,15 +65,22 @@ function Grid({ gridSize }) {
         return circles;
     }
 
+    function handleClick(line) {
+        console.log(line, line.neighbours);
+        // check if it's a box; if box => +1 point.
+    }
+
     return (
         <div className="Grid">
-            {
-                gridCells.map(row => (row.map(cell => (
-                    < div className={`line ${cell.isHorisontal ? "h-line" : "v-line"}`} style={{
-                        top: `${gridSize * cell.y + 2.5}rem`,
-                        left: `${gridSize * 2 * cell.x + 2.5}rem`,
+            {//draws lines
+                gridLines.map(row => (row.map(line => (
+                    < div className={`line ${line.isHorisontal ? "h-line" : "v-line"}`} style={{
+                        top: `${gridSize * line.y + 2.5}rem`,
+                        left: `${gridSize * 2 * line.x + 2.5}rem`,
                         width: `${gridSize * 2}rem`
-                    }} key={Math.random() * 10000}> </div>
+                    }}
+                        onClick={() => handleClick(line)}
+                        key={Math.random() * 10000}></div>
                 ))))
             }
             {drawDots()}
