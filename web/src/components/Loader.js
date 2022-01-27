b63da23be5b63cbad36ff999b85c6406756bd6a1
lemmoor <49235295+lemmoor@users.noreply.github.com>
2022-01-27 22:26:41 +0100

    Fixed an error

--- web/src/components/Loader.js
+++ web/src/components/Loader.js
@@ -2,7 +2,7 @@ import styles from "./Loader.module.css"
 
 function Loader() {
     return (
-        <div class={styles.loader}>
+        <div className={styles.loader}>
             <div></div>
             <div></div>
             <div></div>
